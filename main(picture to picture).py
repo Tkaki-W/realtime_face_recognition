52e6425b7cbffbdf16213e07321a8ca840b80559
Tkaki-W <takanoda2005@gmail.com>
2025-08-31 18:04:12 +0900

    add the distance

--- main(picture to picture).py
+++ main(picture to picture).py
@@ -1,10 +1,19 @@
 import cv2
 import face_recognition
+import numpy as np
 
 #写真を読み込ませ、特徴量を検出する
-img = cv2.imread("../images/messi.webp")
-rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-img_encoding = face_recognition.face_encodings(rgb_img)[0]
+img1 = cv2.imread("images/Messi.jpg")
+rgb_img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2RGB)
+img1_encoding = face_recognition.face_encodings(rgb_img1)[0]
 
-cv2.imshow("Img" , img)
+img2 = cv2.imread("images/Ronald.jpg")
+rgb_img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2RGB)
+img2_encoding = face_recognition.face_encodings(rgb_img2)[0]
+
+cv2.imshow("Img" , img1)
+cv2.imshow("Img2", img2)
 cv2.waitKey(0)
+
+distance = np.linalg.norm(img1_encoding - img2_encoding)
+print(f"顔の類似度:{distance:.4f}")
--- simple_facerec.py
+++ simple_facerec.py
@@ -48,9 +48,10 @@ class SimpleFacerec:
         face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
         face_names = []
+        best_face_distances = []
         for face_encoding in face_encodings:
             # See if the face is a match for the known face(s)
-            matches = face_recognition.compare_faces(self.known_face_encodings, face_encoding)
+            matches = face_recognition.compare_faces(self.known_face_encodings, face_encoding, tolerance=0.6)
             name = "Unknown"
 
             # # If a match was found in known_face_encodings, just use the first one.
@@ -61,11 +62,13 @@ class SimpleFacerec:
             # Or instead, use the known face with the smallest distance to the new face
             face_distances = face_recognition.face_distance(self.known_face_encodings, face_encoding)
             best_match_index = np.argmin(face_distances)
+            best_face_distance = np.amin(face_distances)
             if matches[best_match_index]:
                 name = self.known_face_names[best_match_index]
             face_names.append(name)
+            best_face_distances.append(best_face_distance)
 
         # Convert to numpy array to adjust coordinates with frame resizing quickly
         face_locations = np.array(face_locations)
         face_locations = face_locations / self.frame_resizing
-        return face_locations.astype(int), face_names
+        return face_locations.astype(int), face_names ,best_face_distances
